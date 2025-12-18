a3847d16640d6ac7e000888ff02c42db8be0e7f0
Lavish2424 <aaramirez589@gmail.com>
2025-12-18 00:08:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ st.markdown("Add, edit, delete, review, and randomly pick Chicago eats!")
 st.sidebar.header("Actions")
 action = st.sidebar.radio(
     "What do you want to do?",
-    ["View All Restaurants",
+    ["View All Restaurant/Bars",
      "Add a Restaurant",
      "Add a Review",
      "Random Pick (with filters)"]
